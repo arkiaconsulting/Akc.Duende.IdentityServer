4c44c97de6b15ff05466c23758deabf543ca5273
Louis <louis@arkiaconsulting.com>
2022-01-09 19:16:41 +0100

    Bootstrap admin Api Nuget project

---
+++ src/Admin/Duende.IdentityServer.Akc.Admin.Api/HttpPipelineExtensions.cs
@@ -0,0 +1,15 @@
+﻿// This code is under Copyright (C) 2021 of Arkia Consulting SARL all right reserved
+
+using Microsoft.AspNetCore.Builder;
+
+namespace Duende.IdentityServer.Akc.Admin.Api
+{
+    public static class HttpPipelineExtensions
+    {
+        public static IApplicationBuilder AddIdentityServerClientApi(this IApplicationBuilder app) =>
+            app.Map("/api/clients", appBuilder =>
+            {
+                appBuilder.UseMiddleware<ClientMiddleware>();
+            });
+    }
+}
---
+++ src/Tests/Duende.IdentityServer.Akc.Admin.Tests/DefaultWebApplicationFactory.cs
@@ -0,0 +1,11 @@
+﻿// This code is under Copyright (C) 2021 of Arkia Consulting SARL all right reserved
+
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Duende.IdentityServer.Akc.Admin.Tests
+{
+    internal class DefaultWebApplicationFactory : WebApplicationFactory<TestProgram>
+    {
+
+    }
+}
