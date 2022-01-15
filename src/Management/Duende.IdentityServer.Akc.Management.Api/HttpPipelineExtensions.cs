602e1c92c5546e7cc50e5c88bb4dab43011ec2b5
Louis <louis@arkiaconsulting.com>
2022-01-15 10:27:04 +0100

    Refactor to minimal Apis

--- src/Management/Duende.IdentityServer.Akc.Management.Api/HttpPipelineExtensions.cs
+++ src/Management/Duende.IdentityServer.Akc.Management.Api/HttpPipelineExtensions.cs
@@ -1,15 +1,20 @@
 ﻿// This code is under Copyright (C) 2021 of Arkia Consulting SARL all right reserved
 
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
 
 namespace Duende.IdentityServer.Akc.Management.Api
 {
     public static class HttpPipelineExtensions
     {
-        public static IApplicationBuilder AddIdentityServerClientApi(this IApplicationBuilder app) =>
-            app.Map("/api/clients", appBuilder =>
-            {
-                appBuilder.UseMiddleware<ClientMiddleware>();
-            });
+        public static IEndpointRouteBuilder AddIdentityServerClientApi(this IEndpointRouteBuilder app)
+        {
+            app.MapGet(Constants.Paths.Clients, ClientMiddleware.Get);
+            app.MapPut($"{Constants.Paths.Clients}/{{clientId}}", ClientMiddleware.Create);
+            app.MapPost($"{Constants.Paths.Clients}/{{clientId}}", ClientMiddleware.Update);
+            app.MapDelete($"{Constants.Paths.Clients}/{{clientId}}", ClientMiddleware.Delete);
+
+            return app;
+        }
     }
 }
---
+++ src/Management/Duende.IdentityServer.Akc.Management.Api/TaskExtensions.cs
@@ -0,0 +1,9 @@
+﻿// This code is under Copyright (C) 2021 of Arkia Consulting SARL all right reserved
+
+namespace Duende.IdentityServer.Akc.Management.Api
+{
+    internal static class TaskExtensions
+    {
+        public static Task<T> AsTask<T>(this T source) => Task.FromResult(source);
+    }
+}
